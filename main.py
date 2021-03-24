9749a35679ccd896ff596e33c6f4e114f66e4605
alex_perez <alieperez96@gmail.com>
2021-03-24 04:43:55 -0400

    finished

--- main.py
+++ main.py
@@ -2,18 +2,75 @@ import discord
 import os
 import requests as req
 from dotenv import load_dotenv  
+from pokemontcgsdk import Type, Rarity, Subtype
 
-def get_response(detail,query=None):
-    if query!=None:
-        response = req.get("https://api.pokemontcg.io/v2/"+ detail +query)
+def get_query(message):
+   query="?q=supertype:pokemon"
+   for i in message:
+       query=query+" "+i
+   return query
+
+def get_data(query):
+    json = get_json(query)
+    data=list(json.values())[0]
+    if len(data) >= 10:
+        return 0
+    if not data:
+        return None
     else:
-        response = req.get("https://api.pokemontcg.io/v2/"+ detail)
-        if response.status_code != 200:
-            raise ApiError('GET /tasks/ {}'.format(response.status_code))
-        else: 
-            return response.json()
-        
+        return data
+
+def get_json(query):
+    response = req.get("https://api.pokemontcg.io/v2/cards" + query)
+    if response.status_code != 200:
+        return None
+    else: 
+        return response.json()
+
+def get_image(data):
+    images=[]
+    for i in data:
+        for a,b in i.items():
+            if a=='images':
+                for cock,balls in b.items():
+                    if cock=='small':
+                        images.append(balls)
+    return images
+
+def get_price(data):
+    prices=[]
+    for i in data:
+        for a,b in i.items():
+            if a=='tcgplayer':
+                for cock,balls in b.items():
+                    if (cock=='prices'):
+                        prices.append(list(balls.items())[0])       
+    return prices
+    
+def prettify(prices):
+    pretty=""
+    for b in prices:
+        title=""
+        cardprices=""
+        if type(b) != dict:
+            title+=("     -- %s -- \n" % b.upper())
+            card=title
+        else:
+            for k,v in b.items():
+                if k.lower() != 'directlow':
+                    cardprices+=("%s : $%s  |   " % (k.upper(), v))
+        card+=cardprices
+    return card
 '''
+message="-cardprice types:water"
+message=message.split(" ")
+message.pop(0)
+query= get_query(message)
+data= get_data(query)
+print(data)
+
+'''
+
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 client=discord.Client()
@@ -27,46 +84,38 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('-card'):
-       
-        await message.channel.send()
+    if message.content.startswith("-types"):
+        types = Type.all()
+        await message.channel.send(types)
+
+    if message.content.startswith("-subtypes"):
+        subtypes = Subtype.all()
+        await message.channel.send(subtypes)
+
+    if message.content.startswith("-attributes"):
+        attributes=["set.name:<set name>","name:<pokemon name", "rarity:<rarity>", "subtypes:<subtypes>" , "types:<pokemon type>"]
+        await message.channel.send(attributes)
+
+    if message.content.startswith("-rarity"):
+        rarities = Rarity.all()
+        await message.channel.send(rarities)
+
+    if message.content.startswith('-cardprice'):
+        mes = message.content.split(" ")
+        mes.pop(0)
+        query= get_query(mes)
+        data= get_data(query)
+        if data == 0:
+            await message.channel.send("Result waay too big, narrow down the search")
+        elif not data:
+            await message.channel.send("You fucked something up in your command.. check pinned messages for correct format")
+        else:
+            image = get_image(data)
+            prices= get_price(data)
+            for i in range(len(prices)):
+                card=prettify(prices[i])
+                await message.channel.send(image[i])
+                await message.channel.send(card)
+        
 
 client.run(TOKEN)
-'''
-attributes=["name","type", "subtype", "rarity"]
-
-def get_query(message):
-    if any(word in message for word in attributes):
-        query=
-    query = message.split("name",1)[1]
-    return query
-
-message="-card name:Aerodactyl type:Rare"
-message=message.split(" ")
-print(get_name(message))
-
-def get_sets():
-    setnames=[]
-    fullJson= get_response("sets")
-    for k,v in fullJson.items():
-        if type(v) == int:
-            break
-        else: 
-            for pokeset in v:
-                for a,b in pokeset.items():
-                    if a =='name':
-                        setnames.append(b)
-    return setnames
-
-
-
-def get_subtypes():
-    pass
-
-def get_supertypes():
-    pass
-
-def get_rarities():
-    pass
-
-
